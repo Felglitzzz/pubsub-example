83dac5048a411cc2d01691129f9de91d5c3186cd
Amande Felix <amandeolaoluwa@gmail.com>
2021-02-18 11:03:12 +0100

    fix db cred

--- publisher-server/ormconfig.js
+++ publisher-server/ormconfig.js
@@ -2,16 +2,23 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+let config = {};
+
+if (process.env.NODE_ENV === 'production') {
+  config.url = process.env.DATABASE_URL
+} else {
+  config.host = process.env.PGHOST,
+  config.port = Number.parseInt(process.env.PGPORT, 2),
+  config.username = process.env.PGUSER,
+  config.password = process.env.PGPASSWORD,
+  config.database = `${process.env.PGDATABASE}${
+    process.env.NODE_ENV == 'test' ? '_test' : ''
+  }`
+}
+
 module.exports = {
   type: 'postgres',
-  host: process.env.PGHOST,
-  port: Number.parseInt(process.env.PGPORT, 2),
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  ssl: process.env.NODE_ENV === 'production',
-  database: `${process.env.PGDATABASE}${
-    process.env.NODE_ENV == 'test' ? '_test' : ''
-  }`,
+  ...config,
   entities: ['dist/database/models/*.model.js'],
   logging: process.env.DATABASE_LOGGING,
   synchronize: false,
